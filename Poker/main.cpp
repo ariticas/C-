9f3452b16d5fc5a93dd7a80f33733b8da8998057
Ari Ticas <ariticas10@gmail.com>
2017-11-14 10:15:40 -0800

    Initial Commit

---
+++ Poker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Poker
+//
+//  Created by Ari Ticas on 11/14/17.
+//  Copyright © 2017 Ari Ticas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
